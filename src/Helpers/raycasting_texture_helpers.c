41c5d85622123478a94c2f67bd005326d91feef5
AhmadHirzallah <Ahmad.hirzallah.pro@gmail.com>
2025-11-07 15:54:22 +0300

    fixed texture flipping

--- src/Helpers/raycasting_texture_helpers.c
+++ src/Helpers/raycasting_texture_helpers.c
@@ -6,7 +6,7 @@
 /*   By: ahmad <ahmad@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/07 16:20:00 by ahmad             #+#    #+#             */
-/*   Updated: 2025/11/07 14:35:29 by ahmad            ###   ########.fr       */
+/*   Updated: 2025/11/07 15:53:23 by ahmad            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,9 @@ void	calculate_texture_coords(t_game *game, t_ray *ray)
 		wall_x = game->player.x + ray->perp_wall_dist * ray->dir_x;
 	wall_x -= floor(wall_x);
 	ray->tex_x = (int)(wall_x * (double)ray->texture->width);
-	if (ray->side == 0 && ray->dir_x > 0)
+	if (ray->side == 0 && ray->dir_x < 0)
 		ray->tex_x = ray->texture->width - ray->tex_x - 1;
-	if (ray->side == 1 && ray->dir_y < 0)
+	if (ray->side == 1 && ray->dir_y > 0)
 		ray->tex_x = ray->texture->width - ray->tex_x - 1;
 	ray->step_tex = 1.0 * ray->texture->height / ray->line_height;
 	ray->tex_pos = (ray->draw_start - WIN_HEIGHT / 2
